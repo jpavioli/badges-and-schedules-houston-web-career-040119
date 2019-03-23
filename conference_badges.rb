cd13787d1837a419bfaf4f8b6b6496293ca09eb4
Jusitn Avioli <justin.avioli@gmail.com>
2019-03-23 18:47:53 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -20,7 +20,7 @@ end
 
 def printer(speakers)
   print_me = []
-  print_me << batch_badge_creator(speakers)
-  print_me << assign_rooms(speakers)
+  print_me.push(batch_badge_creator(speakers))
+  print_me.push(assign_rooms(speakers))
   print_me
 end
